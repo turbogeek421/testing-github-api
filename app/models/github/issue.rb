dcdb9efdeda4a09e44b8793c8d70264a154a03c4
Mike Fox <turbogeek421.mike@gmail.com>
2025-07-18 11:09:39 +0100

    feat: adds Flexirest gem and class to fetch Github issue data

---
+++ app/models/github/issue.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Github::Issue < Flexirest::Base
+  base_url Rails.configuration.x.github_api_url
+  request_body_type :json
+
+  before_request :set_headers
+
+  get :updated_since,
+      "/repos/storyblok/storyblok/issues?state=all&sort=updated&since=:date&per_page=100&page=:page",
+      requires: %i[date page]
+
+  private
+    def set_headers(_name, request)
+      request.headers["Accept"] = "application/vnd.github+json"
+    end
+end
--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,7 @@ module TestingGithubApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.x.github_api_url = "https://api.github.com"
   end
 end
